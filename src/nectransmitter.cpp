7de7c6227c80c82e4a95485a8e22ba24f7e78e75
ballisnate <ballisnate@gmail.com>
2023-01-07 16:27:00 -0600

    removing refs to atomic for non-AVR boards

--- src/nectransmitter.cpp
+++ src/nectransmitter.cpp
@@ -1,94 +1,110 @@
 #include "nectransmitter.h"
 
-#include <util/atomic.h>
-
 #define NEC_VALUE_PULSE_DURATION 562
 #define NEC_0_PAUSE_DURATION 562
 #define NEC_1_PAUSE_DURATION 1687
 
 NECTransmitter::NECTransmitter(uint8_t pin, bool activeLow)
     : pin(pin), activeValue(activeLow ? LOW : HIGH),
-      nonActiveValue(activeLow ? HIGH : LOW) {
+      nonActiveValue(activeLow ? HIGH : LOW)
+{
   pinMode(pin, OUTPUT);
 
   // Output non-active value.
   digitalWrite(pin, nonActiveValue);
 }
 
-void NECTransmitter::SendPulse(bool value) const {
+void NECTransmitter::SendPulse(bool value) const
+{
   digitalWrite(pin, activeValue);
   delayMicroseconds(NEC_VALUE_PULSE_DURATION);
   digitalWrite(pin, nonActiveValue);
 
-  if (value) {
+  if (value)
+  {
     delayMicroseconds(NEC_1_PAUSE_DURATION);
-  } else {
+  }
+  else
+  {
     delayMicroseconds(NEC_0_PAUSE_DURATION);
   }
 }
 
-void NECTransmitter::SendByte(uint8_t byte, bool includeInverse) const {
+void NECTransmitter::SendByte(uint8_t byte, bool includeInverse) const
+{
   // Send normal.
-  for (int bit = 0; bit < 8; bit++) {
+  for (int bit = 0; bit < 8; bit++)
+  {
     SendPulse((byte >> bit) & 1);
   }
 
-  if (!includeInverse) {
+  if (!includeInverse)
+  {
     return;
   }
 
   // Send logical inverse.
-  for (int bit = 0; bit < 8; bit++) {
+  for (int bit = 0; bit < 8; bit++)
+  {
     SendPulse(!((byte >> bit) & 1));
   }
 }
 
-void NECTransmitter::SendNEC(uint8_t address, uint8_t command) const {
+void NECTransmitter::SendNEC(uint8_t address, uint8_t command) const
+{
+  // Disable interrupts.
+  noInterrupts();
 
-  // Ensure we run atomically.
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) { // Send AGC (9ms pulse).
-    digitalWrite(pin, activeValue);
-    delayMicroseconds(9000);
+  // Send AGC (9ms pulse).
+  digitalWrite(pin, activeValue);
+  delayMicroseconds(9000);
 
-    // Send space.
-    digitalWrite(pin, nonActiveValue);
-    delayMicroseconds(4500);
+  // Send space.
+  digitalWrite(pin, nonActiveValue);
+  delayMicroseconds(4500);
 
-    // Send address.
-    SendByte(address);
+  // Send address.
+  SendByte(address);
 
-    // Send code.
-    SendByte(command);
+  // Send code.
+  SendByte(command);
 
-    // End.
-    SendPulse(false);
+  // End.
+  SendPulse(false);
 
-    // Reset to non active.
-    digitalWrite(pin, nonActiveValue);
-  }
+  // Reset to non active.
+  digitalWrite(pin, nonActiveValue);
+
+  // Reenable interrupts
+  interrupts();
 }
 
-void NECTransmitter::SendExtendedNEC(uint16_t address, uint8_t command) const {
-  // Ensure we run atomically.
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) { // Send AGC (9ms pulse).
-    digitalWrite(pin, activeValue);
-    delayMicroseconds(9000);
+void NECTransmitter::SendExtendedNEC(uint16_t address, uint8_t command) const
+{
+  // Disable interrupts.
+  noInterrupts();
+  
+  // Send AGC (9ms pulse).
+  digitalWrite(pin, activeValue);
+  delayMicroseconds(9000);
 
-    // Send space.
-    digitalWrite(pin, nonActiveValue);
-    delayMicroseconds(4500);
+  // Send space.
+  digitalWrite(pin, nonActiveValue);
+  delayMicroseconds(4500);
 
-    // Send address.
-    SendByte(address / 256, /* includeInverse */ false);
-    SendByte(address & 0xff, /* includeInverse */ false);
+  // Send address.
+  SendByte(address / 256, /* includeInverse */ false);
+  SendByte(address & 0xff, /* includeInverse */ false);
 
-    // Send code.
-    SendByte(command);
+  // Send code.
+  SendByte(command);
 
-    // End.
-    SendPulse(false);
+  // End.
+  SendPulse(false);
 
-    // Reset to non active.
-    digitalWrite(pin, nonActiveValue);
-  }
+  // Reset to non active.
+  digitalWrite(pin, nonActiveValue);
+
+  // Enable interrupts
+  interrupts();
 }
